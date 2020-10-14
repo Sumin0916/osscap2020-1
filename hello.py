36dbc581f81f5ee75a7f0c8ad7886c5c2ddea3d1
Sumin Kang <sumin@DESKTOP-TG7V0PE.localdomain>
2020-10-14 15:20:06 +0900

    hello.py edited(by.sumin)

--- hello.py
+++ hello.py
@@ -1,5 +1,6 @@
 print("hello world!")
 print("hello world!!")
+print("hello world~~!")
 #과제를 위해 main_1 branch에서 추가되고 merge됨  -20190298 최승재test
 #과제를 위해 main2  branch 생성함                -20201793 김준성
-
+#과제를 위해 main3  branch 생성함                -20203063 강수민
