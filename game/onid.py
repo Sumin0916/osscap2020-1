f0f20e77e2f57e0c93c95e62aa4f7f76b5facd09
jikukjs <37741469+jikukjs@users.noreply.github.com>
2020-11-18 17:07:38 +0900

    게임기능 추가
    - 이미지 수정
    - 키보드 입력 추가 
    - 게임 진행 방식 변경
    
    -> 체력바, 보스, 이미지 (주인공, 보스), 점수
    -> 메뉴, 이미지 도트 작업

--- game/onid.py
+++ game/onid.py
@@ -3,13 +3,15 @@ import sys
 import time
 import random
 
-image_load_list = [["walk_1","걷기 1", (80, 80)], ["walk_2","걷기 2", (80, 80)], ["dino_image", "dino", (80,80)], ["obstacle_1", "가위괴물", (100, 100)], ["obstacle_2", "바위괴물", (100, 100)], ["obstacle_3", "보괴물", (100, 100)],  ["t_gameover", "게임오버_글자", (480,30)], ["gameover_image", "게임오버 다시하기", (88, 80)], ["die_image", "dino die" ,(80, 80)]]
+image_load_list = [["walk_1", "걷기 1", (80, 80)], ["walk_2", "걷기 2", (80, 80)], ["dino_image", "dino", (80, 80)],
+                   ["obstacle_1", "가위괴물", (100, 100)], ["obstacle_2", "바위괴물", (100, 100)],
+                   ["obstacle_3", "보괴물", (100, 100)], ["t_gameover", "게임오버_글자", (480, 30)],
+                   ["gameover_image", "게임오버 다시하기", (88, 80)], ["die_image", "dino die", (80, 80)]]
 
 for image_load in image_load_list:
     globals()[image_load[0]] = pg.transform.scale(pg.image.load("{}.PNG".format(image_load[1])), image_load[2])
 
-
-random_obstacle_list = [(obstacle_1, (120, 100)), (obstacle_2, (90, 90)), (obstacle_3, (50, 100)) ]
+random_obstacle_list = [(obstacle_1, (100, 100)), (obstacle_2, (100, 100)), (obstacle_3, (100, 100))]
 
 screen = pg.display.set_mode((1000, 500))
 pg.display.set_caption("Open source 9")
@@ -22,8 +24,9 @@ dust_list = []
 
 best_score = 0
 
+
 def game_set():
-    global walk_image, dino_x, dino_y, walk_image2, dust_list, game_speed , speed_up,  jump, jump_speed, add_obstacle, obstacle_list, die
+    global walk_image, dino_x, dino_y, walk_image2, dust_list, game_speed, speed_up, jump, jump_speed, add_obstacle, obstacle_list, die
     walk_image = 0
     dino_x = 50
     dino_y = 380
@@ -35,6 +38,8 @@ def game_set():
     add_obstacle = 0
     obstacle_list = []
     die = False
+
+
 
 
 def d_dino():
@@ -52,45 +57,67 @@ def d_dino():
             if walk_image2 % 10 == 0:
                 walk_image += 1
 
+
 def u_dino():
-    global  walk_image2
+    global walk_image2
     walk_image2 += 1
 
+
 def d_background():
-    screen.fill((255,255,255))
-    pg.draw.line(screen,(83,83,83),(0, 450), (1000, 450), 1)
+    screen.fill((255, 255, 255))
+    pg.draw.line(screen, (83, 83, 83), (0, 450), (1000, 450), 1)
     for dust in dust_list:
-        pg.draw.line(screen,(83,83,83) ,(dust[0], dust[1]), (dust[0] + dust[2], dust[1]))
+        pg.draw.line(screen, (83, 83, 83), (dust[0], dust[1]), (dust[0] + dust[2], dust[1]))
 
 
 def u_obstacle():
     global add_obstacle
-    if add_obstacle > random.randint(300, 400): # (obstacle_1, (150, 100))
-        random_list = random_obstacle_list[random.randint(0, 3)]
-        obstacle_list.append([random_list[0],[1000, random.randint(460, 470) - random_list[1][1]], random_list])
+    if add_obstacle > random.randint(300, 400):  # (obstacle_1, (150, 100))
+        random_list = random_obstacle_list[random.randint(0, 2)]
+        obstacle_list.append([random_list[0], [1000, random.randint(460, 470) - random_list[1][1]], random_list])
         add_obstacle = 0
+        action()
+
     for obstacle in obstacle_list:
-        if obstacle[1][0] < -200:
-            obstacle_list.remove(obstacle)
+        if obstacle[2] == random_obstacle_list[0]:
+            if action() == "rock" :
+                obstacle_list.remove(obstacle)
+
+        if obstacle[2] == random_obstacle_list[1]:
+            if action() == "paper" :
+                obstacle_list.remove(obstacle)
+
+
+        if obstacle[2] == random_obstacle_list[2]:
+            if action() == "sicssor" :
+                obstacle_list.remove(obstacle)
+
         else:
             obstacle[1][0] -= game_speed
+
+
     add_obstacle += 1
-    
 
 
 def d_obstacle():
     for obstacle in obstacle_list:
         screen.blit(obstacle[0], obstacle[1])
 
+
 def d_gameover():
-    screen.blit(t_gameover ,(260, 154))
+    screen.blit(t_gameover, (260, 154))
     screen.blit(gameover_image, (446, 240))
+
 
 def check_collision():
     for collision_xy in collision_list:
         for obstacle in obstacle_list:
-            if dino_x + collision_xy[0] > obstacle[1][0] and dino_x + collision_xy[0] < obstacle[1][0] + obstacle[2][1][0] and dino_y + collision_xy[1] > obstacle[1][1] and dino_y + collision_xy[1] < obstacle[1][1] + obstacle[2][1][1]:
+            if dino_x + collision_xy[0] > obstacle[1][0] and dino_x + collision_xy[0] < obstacle[1][0] + obstacle[2][1][
+                0] and dino_y + collision_xy[1] > obstacle[1][1] and dino_y + collision_xy[1] < obstacle[1][1] + \
+                    obstacle[2][1][1]:
                 return True
+
+
 def speed():
     global jump, jump_speed, speed_up, game_speed, dino_y
     if jump:
@@ -106,9 +133,18 @@ def speed():
             game_speed += 1
     speed_up += 1
 
-def scissor():
-    if obstacle[0] ==  obstacle_1 :
-        
+def action():
+    for event in pg.event.get():
+        if event.type == pg.KEYDOWN:
+            if event.key == pg.K_LEFT:
+                return "sicssor"
+
+            if event.key == pg.K_DOWN:
+                return "rock"
+
+            if event.key == pg.K_RIGHT:
+                return "paper"
+
 
 
 game_set()
@@ -119,13 +155,7 @@ while True:
             running = False
             pg.quit()
             sys.quit()
-        if event.type == pg.KEYDOWN:
-            if event.key == pg.K_RIGHT:
-                if not die:
-                    scissor()
-                else:
-                    die = False
-                    game_set()
+
 
     u_dino()
     d_background()
