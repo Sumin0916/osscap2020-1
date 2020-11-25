53f9233b62f699f6f31253899406a1b4c8971001
caretop <65876996+caretop@users.noreply.github.com>
2020-11-26 04:19:47 +0900

    Update hand_recog.py
    -인식 범위를 특정 사각형 안으로 좁힘으로써 인식 정확도 상승
    -contour과 hull을 그대로 사용하지 않고  contour 구역을 설정하고 & approxPolyDP를 사용하여 defects들이 튀는 현상을 감소

--- working_game/hand_recog.py
+++ working_game/hand_recog.py
@@ -1,83 +1,123 @@
 import numpy as np
+import math
+import time
 import cv2 as cv
+import imutils
 from datetime import datetime, timedelta # 일정 시간동안 동작인식 함수를 작동시킴
-import collections  #collections.counter을 이용해 인식한 동작 수를 셈 
 
-def skinmask(img):
-    hsvim = cv.cvtColor(img, cv.COLOR_BGR2HSV)  #(입력받는 B/G/R이미지를 hsv=색상/채도/명도 이미지로 변경)
-    lower = np.array([0, 48, 80], dtype = "uint8") #HSV 로 봤을 때 피부색의 최저치
-    upper = np.array([20, 255, 255], dtype = "uint8") #HSV 로 봤을 때 피부색의 최대치
-    skinRegionHSV = cv.inRange(hsvim, lower, upper) #lower와 upper값 사이로 피부색 영역 검출
-    blurred = cv.blur(skinRegionHSV, (2,2)) #피부색 영역을 블러처리함 
-    ret, thresh = cv.threshold(blurred,0,255,cv.THRESH_BINARY) # 피부색 이미지 2진수화(임계화)
-    return thresh
 
-def getcnthull(mask_img): #컨투어 검출 
-    contours, hierarchy = cv.findContours(mask_img, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
-    contours = max(contours, key=lambda x: cv.contourArea(x))
-    hull = cv.convexHull(contours) #컨투어의 끝점들 연결 
-    return contours, hull
+def skinmask(roi):
+    kernel = np.ones((3,3),np.uint8)  
+    hsv = cv.cvtColor(roi, cv.COLOR_BGR2HSV)
+    lower = np.array([0,48,80], dtype=np.uint8)
+    upper = np.array([20,255,255], dtype=np.uint8)
 
-def getdefects(contours): #검출한 손가락 끝점들(hull)을 이용해 손가락 사이들 알아내기 
-    hull = cv.convexHull(contours, returnPoints=False)
-    defects = cv.convexityDefects(contours, hull)
-    return defects
+    mask = cv.inRange(hsv, lower, upper)
+    mask = cv.dilate(mask,kernel,iterations = 4)
+    mask = cv.GaussianBlur(mask,(5,5),100) 
+    return mask 
+       
+def getcnt(mask_frame):
+    contours,hierarchy= cv.findContours(mask_frame,cv.RETR_TREE,cv.CHAIN_APPROX_SIMPLE)
+    cnt = max(contours, key = lambda x: cv.contourArea(x))
+    return cnt
+
+def getHullnDefects(cnt,approx):
+    hull = cv.convexHull(cnt)
+
+    areahull = cv.contourArea(hull)
+    areacnt = cv.contourArea(cnt)
+    arearatio=((areahull-areacnt)/areacnt)*100
+
+    hull = cv.convexHull(approx, returnPoints=False)
+    defects = cv.convexityDefects(approx, hull)
+
+    return hull, defects, arearatio, areacnt 
 
 
 def rps():
-    
-    ip_list=[]#5초간 인식한 동작들을 저장함(rck, ppr, scr)
     cap = cv.VideoCapture(0) # '0' for webcam
-    end_time = datetime.now() + timedelta(seconds=5) #5초간 입력을 받음 
-    while cap.isOpened() and datetime.now() < end_time:
-        _, img = cap.read()
+    end_time = datetime.now() + timedelta(seconds=110) #10초간 입력을 받음 
+    while (1) and datetime.now() < end_time:
         try:
-            mask_img = skinmask(img)
-            contours, hull = getcnthull(mask_img)
-            cv.drawContours(img, [contours], -1, (255,255,0), 2)
-            cv.drawContours(img, [hull], -1, (0, 255, 255), 2)
-            defects = getdefects(contours)
-            if defects is not None:
-                cnt = 0
-                for i in range(defects.shape[0]):  # 디펙트들 사이의 각도들 측정 
-                    s, e, f, d = defects[i][0] #(시작점, 끝점, 시작점에서 가장 먼 점, 가장 먼 점까지의 거리)
-                    start = tuple(contours[s][0])
-                    end = tuple(contours[e][0])
-                    far = tuple(contours[f][0])
-                    a = np.sqrt((end[0] - start[0]) ** 2 + (end[1] - start[1]) ** 2)
-                    b = np.sqrt((far[0] - start[0]) ** 2 + (far[1] - start[1]) ** 2)
-                    c = np.sqrt((end[0] - far[0]) ** 2 + (end[1] - far[1]) ** 2)
-                    angle = np.arccos((b ** 2 + c ** 2 - a ** 2) / (2 * b * c))  #     코사인값 구하기 
-                    if angle <= np.pi / 2:  #각도가 90도 이하면 손가락으로 취급 
-                        cnt += 1
-                        cv.circle(img, far, 4, [0, 0, 255], -1)
-                if cnt > 0:
-                    cnt = cnt+1
-                #cv.putText(img, str(cnt), (0, 50), cv.FONT_HERSHEY_SIMPLEX,1, (255, 0, 0) , 2, cv.LINE_AA)
+            ret, frame = cap.read()
+            frame=cv.flip(frame,1)
+            roi=frame[100:300, 100:300]  
+            cv.rectangle(frame,(100,100),(300,300),(0,255,0),0)  
+
+            mask_frame = skinmask(roi)
+
+            cnt= getcnt(mask_frame)
+            #형상을 approximate 하여 잡티를 없앰 
+            epsilon = 0.0005*cv.arcLength(cnt,True)
+            approx= cv.approxPolyDP(cnt,epsilon,True)
+
+            hull, defects, arearatio, areacnt = getHullnDefects(cnt,approx)
+
+            fngr=0
+        
+    
+            for i in range(defects.shape[0]):
+                s,e,f,d = defects[i,0]
+                start = tuple(approx[s][0])
+                end = tuple(approx[e][0])
+                far = tuple(approx[f][0])
+                pt= (100,180)
+                
             
-            if cnt >=2 and cnt <5:
-                #print("Scissors")
-                cv.putText(img, str("scr"), (0, 50), cv.FONT_HERSHEY_SIMPLEX,1, (255, 0, 0) , 2, cv.LINE_AA)
-                ip_list.append('scr')
-                #return str("scissor")
-            elif cnt>=5:
-                cv.putText(img, str("paper"), (0, 50), cv.FONT_HERSHEY_SIMPLEX,1, (255, 0, 0) , 2, cv.LINE_AA)
-                ip_list.append('ppr')
-                #return str("paper")
-            else:
-                cv.putText(img, str("Rck"), (0, 50), cv.FONT_HERSHEY_SIMPLEX,1, (255, 0, 0) , 2, cv.LINE_AA)
-                ip_list.appned('rck')
-                #return str("wha?")
-            
-            cv.imshow("img", img)
+                a = math.sqrt((end[0] - start[0])**2 + (end[1] - start[1])**2)
+                b = math.sqrt((far[0] - start[0])**2 + (far[1] - start[1])**2)
+                c = math.sqrt((end[0] - far[0])**2 + (end[1] - far[1])**2)
+                s = (a+b+c)/2
+                ar = math.sqrt(s*(s-a)*(s-b)*(s-c))
+                
+                d=(2*ar)/a
+                
+                angle = math.acos((b**2 + c**2 - a**2)/(2*b*c)) * 57
+                
+                if angle <= 90 and d>30:
+                    fngr += 1
+                    cv.circle(roi, far, 3, [255,0,0], -1)
+                
+                cv.line(roi,start, end, [0,255,0], 2)
+                
+                
+            fngr+=1
+
+            font = cv.FONT_HERSHEY_SIMPLEX
+            if fngr==1:
+                if areacnt<2000:
+                    cv.putText(frame,'Put your hand in the box',(0,50), font, 2, (0,0,255), 3, cv.LINE_AA)
+                else:
+                    if arearatio<12:
+                        cv.putText(frame,'Rock',(0,50), font, 2, (0,0,255), 3, cv.LINE_AA)
+                        
+            elif fngr==2:
+                cv.putText(frame,'Scissors',(0,50), font, 2, (0,0,255), 3, cv.LINE_AA)
+                
+            elif fngr==5 or fngr ==4:
+                cv.putText(frame,'Papers',(0,50), font, 2, (0,0,255), 3, cv.LINE_AA)
+                
+            elif fngr==6:
+                cv.putText(frame,'reposition',(0,50), font, 2, (0,0,255), 3, cv.LINE_AA)
+                
+            else :
+                cv.putText(frame,'reposition',(10,50), font, 2, (0,0,255), 3, cv.LINE_AA)
+                
+        #show the windows
+            print("dsd")
+            cv.imshow('mask',mask_frame)
+            cv.imshow('frame',frame)
+        
         except:
             pass
-        if cv.waitKey(1) & 0xFF == ord('q'):
+    
+        k = cv.waitKey(5) & 0xFF
+        if k == 27:
             break
-            
-    print(collections.Counter(ip_list))
-    cap.release()
+    
     cv.destroyAllWindows()
+    cap.release()  
 
 if __name__ == "__main__":
     rps()
