e7fbf823914a4a39760645f0893089cb7d04ce80
caretop <65876996+caretop@users.noreply.github.com>
2020-11-25 17:52:35 +0900

    Update hand_recog.py
    주석 추가

--- working_game/hand_recog.py
+++ working_game/hand_recog.py
@@ -12,19 +12,20 @@ def skinmask(img):
     ret, thresh = cv.threshold(blurred,0,255,cv.THRESH_BINARY) # 피부색 이미지 2진수화(임계화)
     return thresh
 
-def getcnthull(mask_img):
+def getcnthull(mask_img): #컨투어 검출 
     contours, hierarchy = cv.findContours(mask_img, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
     contours = max(contours, key=lambda x: cv.contourArea(x))
-    hull = cv.convexHull(contours)
+    hull = cv.convexHull(contours) #컨투어의 끝점들 연결 
     return contours, hull
 
-def getdefects(contours):
+def getdefects(contours): #검출한 손가락 끝점들(hull)을 이용해 손가락 사이들 알아내기 
     hull = cv.convexHull(contours, returnPoints=False)
     defects = cv.convexityDefects(contours, hull)
     return defects
 
 
 def rps():
+    
     ip_list=[]#5초간 인식한 동작들을 저장함(rck, ppr, scr)
     cap = cv.VideoCapture(0) # '0' for webcam
     end_time = datetime.now() + timedelta(seconds=5) #5초간 입력을 받음 
@@ -38,16 +39,16 @@ def rps():
             defects = getdefects(contours)
             if defects is not None:
                 cnt = 0
-                for i in range(defects.shape[0]):  # calculate the angle
-                    s, e, f, d = defects[i][0]
+                for i in range(defects.shape[0]):  # 디펙트들 사이의 각도들 측정 
+                    s, e, f, d = defects[i][0] #(시작점, 끝점, 시작점에서 가장 먼 점, 가장 먼 점까지의 거리)
                     start = tuple(contours[s][0])
                     end = tuple(contours[e][0])
                     far = tuple(contours[f][0])
                     a = np.sqrt((end[0] - start[0]) ** 2 + (end[1] - start[1]) ** 2)
                     b = np.sqrt((far[0] - start[0]) ** 2 + (far[1] - start[1]) ** 2)
                     c = np.sqrt((end[0] - far[0]) ** 2 + (end[1] - far[1]) ** 2)
-                    angle = np.arccos((b ** 2 + c ** 2 - a ** 2) / (2 * b * c))  #      cosine theorem
-                    if angle <= np.pi / 2:  # angle less than 90 degree, treat as fingers
+                    angle = np.arccos((b ** 2 + c ** 2 - a ** 2) / (2 * b * c))  #     코사인값 구하기 
+                    if angle <= np.pi / 2:  #각도가 90도 이하면 손가락으로 취급 
                         cnt += 1
                         cv.circle(img, far, 4, [0, 0, 255], -1)
                 if cnt > 0:
@@ -73,8 +74,8 @@ def rps():
             pass
         if cv.waitKey(1) & 0xFF == ord('q'):
             break
+            
     print(collections.Counter(ip_list))
-
     cap.release()
     cv.destroyAllWindows()
 
